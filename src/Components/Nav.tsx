e9370d9d71ce48c2b6c496763a47cabb78461e0e
gwonyeong <qwe980121@naver.com>
2023-11-14 22:19:11 +0900

    Add: 햄버거 눌렀을 때 메뉴모달 추가

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useState } from 'react';
-import ReactModal from 'react-modal';
+import { useCallback, useState } from 'react';
+
 import freindIcon from 'Assets/Images/freind.png';
-import FrenidModal from './Modals/FrenidModal';
+import FrenidModal from './Modals/Freind/FrenidModal';
+import MenuModal from './Modals/Menu/Menu';
 
 export default function Nav() {
     const [isFreindModal, setIsFreindModal] = useState<boolean>(false);
@@ -21,6 +22,8 @@ export default function Nav() {
             {isFreindModal && (
                 <FrenidModal handleIsFreindModal={handleIsFreindModal} />
             )}
+            {/* 메뉴 모달 */}
+            {isMenuModal && <MenuModal handleIsMenuModal={handleIsMenuModal} />}
             <div className="flex flex-row justify-between items-center px-5 py-3">
                 <div
                     className="flex flex-row items-center"
@@ -28,7 +31,11 @@ export default function Nav() {
                         handleIsFreindModal();
                     }}
                 >
-                    <img src={freindIcon} className="w-8 h-8" />
+                    <img
+                        src={freindIcon}
+                        className="w-8 h-8"
+                        alt={'freing icon'}
+                    />
                 </div>
                 {/* 햄버거 버튼 */}
                 <div
