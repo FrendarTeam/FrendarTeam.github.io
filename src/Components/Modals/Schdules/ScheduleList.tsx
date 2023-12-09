72cf0db10445caab8b8ceab56174be8c2e13b8c4
gwonyeong <qwe980121@naver.com>
2023-12-09 23:55:45 +0900

    Add: Schedule Mock data

--- src/Components/Modals/Schdules/ScheduleList.tsx
+++ src/Components/Modals/Schdules/ScheduleList.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import { ScheduleData } from 'Types/Schedule/scheduleData'
+import React, { useEffect, useState } from 'react'
 
 export default function ScheduleList() {
+    const [scheduleData, setScheduleData] = useState<ScheduleData>()
+
+    useEffect(() => {
+        const getScheduleData = async () => {
+            // const scheduleData = await ScheduleData.getScheduleData()
+            const mockData: ScheduleData = {
+                title: 'title',
+                location: 'location',
+                startTime: new Date(),
+                endTime: new Date(),
+                isPrivate: false,
+                freinds: [
+                    {
+                        nickname: 'user1',
+                    },
+                    {
+                        nickname: 'user2',
+                    },
+                ],
+            }
+            setScheduleData(mockData)
+        }
+        getScheduleData()
+    }, [])
+
+    if (!scheduleData) return <></>
+
     return (
         <div className="flex w-full flex-col items-center">
-            <div className="flex  flex-col w-4/5 bg-slate-400 ">
+            <div className="flex  flex-col w-4/5 ">
                 <p>Title</p>
                 <p
                     style={{
                         fontSize: '30px',
                     }}
                 >
-                    김려은 좋아해
+                    {scheduleData.title}
                 </p>
             </div>
         </div>
